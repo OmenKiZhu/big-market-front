495aaca19365f494d37041b8afa5d965b11a3680
朱兆麒 <masin2001zzq@163.com>
2024-10-23 09:31:27 +0800

    feat:第3节：应用接口对接抽奖页面

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,44 +1,40 @@
 // 请求地址
-const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "https://apifoxmock.com/m1/5110192-4772993-default";
+const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "https://console-mock.apipost.cn/mock/6afa907d-6678-45e2-b867-032a11090abd";
+
+/**
+ * 装配抽奖
+ * @param strategyId
+ */
+export const strategyArmory = (strategyId?: number) => {
+    return fetch(`${apiHostUrl}/api/v1/raffle/strategy_armory?strategyId=${strategyId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
 
 /**
  * 查询抽奖奖品列表
  * @param strategyId 策略ID
  */
-
-export const queryRaffleAwardList = (strategyId: number) => {
-    try{
-        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list?strategyId=${strategyId}`, {
-            method: 'get',
+export const queryRaffleAwardList = (strategyId?: number) => {
+    try {
+        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list`, {
+            method: 'POST',
             headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                strategyId: strategyId
+            })
         });
-    }catch (error){
+    } catch (error) {
         return fetch("{\n" +
             "    \"code\": \"0001\",\n" +
             "    \"info\": \"调用失败\",\n" +
             "    \"data\": [\n" +
             "}");
-
-    }
-}
-
-export const queryRaffleAwardListByGird = (strategyId: number) => {
-    try{
-        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list_Grid?strategyId=${strategyId}`, {
-            method: 'get',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
-        });
-    }catch (error){
-        return fetch("{\n" +
-            "    \"code\": \"0001\",\n" +
-            "    \"info\": \"调用失败\",\n" +
-            "    \"data\": [\n" +
-            "}");
-
     }
 }
 
@@ -56,30 +52,16 @@ export const queryRaffleAwardListByGird = (strategyId: number) => {
  * 	}
  * }
  */
-export const randomRaffle = (strategyId: number) => {
+export const randomRaffle = (strategyId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle?strategyId=${strategyId}`, {
-            method: 'get',
+        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
-            }
-        })
-    } catch (error) {
-        return fetch("{\n" +
-            "    \"code\": \"0001\",\n" +
-            "    \"info\": \"调用失败\",\n" +
-            "    \"data\": [\n" +
-            "}");
-    }
-}
-
-export const randomRaffleByGrid = (strategyId: number) => {
-    try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle_grid?strategyId=${strategyId}`, {
-            method: 'get',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
+            },
+            body: JSON.stringify({
+                strategyId: strategyId
+            })
         })
     } catch (error) {
         return fetch("{\n" +
